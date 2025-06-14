57fc884b896858f207fac0cf6dbd7bdeb676db6d
Mateusz <matid@spoko.pl>
2025-06-14 16:47:49 +0200

    Made secret not so secret

---
+++ secret.js
@@ -0,0 +1,4 @@
+const SECRET = {
+  API_URL: 'https://craxqjxixhzwrzqrnvju.supabase.co',
+  API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyYXhxanhpeGh6d3J6cXJudmp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMzA2ODgsImV4cCI6MjA2NDkwNjY4OH0.wFbarwUJowhT93Rcf6MfbrrAQjHXF8GjxYzNak-pmMo',
+  };
